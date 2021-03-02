023e6ff81e5bcd039e033476cb88f4411cf4fb75
hualeel <hualeel@sina.com>
2021-03-02 14:50:20 +0800

    Update 1-system-init.sh

--- 1-system-init.sh
+++ 1-system-init.sh
@@ -40,25 +40,3 @@ echo 'vm.drop_caches=3' >> /etc/sysctl.conf
 sysctl -p
 ulimit -c 0 && echo 'ulimit -S -c 0' >>/etc/profile
 modprobe iptable_nat
-
-#追加修改内核参数
-cat > kubernetes.conf <<EOF
-#net.bridge.bridge-nf-call-iptables=1
-#net.bridge.bridge-nf-call-ip6tables=1
-net.ipv4.ip_forward=1
-net.ipv4.tcp_tw_recycle=0
-net.ipv4.neigh.default.gc_thresh1=1024
-net.ipv4.neigh.default.gc_thresh1=2048
-net.ipv4.neigh.default.gc_thresh1=4096
-vm.swappiness=0
-vm.overcommit_memory=1
-vm.panic_on_oom=0
-fs.inotify.max_user_instances=8192
-fs.inotify.max_user_watches=1048576
-fs.file-max=52706963
-fs.nr_open=52706963
-net.ipv6.conf.all.disable_ipv6=1
-net.netfilter.nf_conntrack_max=2310720
-EOF
-cp kubernetes.conf  /etc/sysctl.d/kubernetes.conf
-sysctl -p /etc/sysctl.d/kubernetes.conf

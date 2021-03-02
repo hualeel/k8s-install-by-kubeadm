0f95e65eab927e34f85c1865d11a2b24dc8b56b4
hualeel <hualeel@sina.com>
2021-03-02 21:41:44 +0800

    Update 2-install-docker.sh

--- 2-install-docker.sh
+++ 2-install-docker.sh
@@ -1,4 +1,5 @@
-cat > /etc/yum.repos.d/docker-ce.repo <<EOF
+#!/bin/bash
+cat > /etc/yum.repos.d/docker-ce.repo <<-EOF
 [docker-ce-stable]
 name=Docker CE Stable Mirror Repository
 baseurl=https://mirrors.aliyun.com/docker-ce/linux/centos/7/x86_64/stable
@@ -6,9 +7,7 @@ enabled=0
 gpgcheck=1
 gpgkey=https://mirrors.aliyun.com/docker-ce/linux/centos/gpg
 EOF
- 
-yum install --enablerepo=docker-ce-stable -y docker-ce-18.06.1.ce
- 
+yum install --enablerepo=docker-ce-stable -y docker-ce
 # 配置加速器
 cat > /etc/docker/daemon.json <<-EOF
 {
@@ -21,7 +20,6 @@ cat > /etc/docker/daemon.json <<-EOF
     ]
 }
 EOF
-
 systemctl daemon-reload
 systemctl enable docker
 systemctl start docker

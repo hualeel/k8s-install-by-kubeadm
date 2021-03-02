3cdc0555c542e663455f41502fa82ea52c665514
hualeel <hualeel@sina.com>
2021-03-02 22:09:05 +0800

    Update 3-setup-k8s-yum-repo.sh

--- 3-setup-k8s-yum-repo.sh
+++ 3-setup-k8s-yum-repo.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/Centos-7.repo
  

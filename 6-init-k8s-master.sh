d2abbeb9034e2e52e372eb279fa330ba158f5749
hualeel <hualeel@sina.com>
2021-03-02 21:56:53 +0800

    Update 6-init-k8s-master.sh

--- 6-init-k8s-master.sh
+++ 6-init-k8s-master.sh
@@ -1,1 +1,6 @@
-kubeadm init --image-repository=registry.aliyuncs.com/google_containers --pod-network-cidr=10.244.0.0/16 --kubernetes-version=v1.20.4
+kubeadm init \
+--apiserver-advertise-address=192.168.0.68 \
+--image-repository=registry.aliyuncs.com/google_containers \
+--kubernetes-version=v1.19.4 \
+--service-cidr=10.96.0.0/12 \
+--pod-network-cidr=10.244.0.0/16

03b421c5ebf710c891fca644ac75d99b96746a00
hualeel <hualeel@sina.com>
2021-08-31 00:07:38 +0800

    Update 5-install-kueadm-kubectl-kubelet-by-yum.sh

--- 5-install-kueadm-kubectl-kubelet-by-yum.sh
+++ 5-install-kueadm-kubectl-kubelet-by-yum.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-yum install -y kubeadm-1.19.8 kubectl-1.19.8 kubelet-1.19.8 --disableexcludes=kubernetes
+yum install -y kubeadm-1.20.5 kubectl-1.20.5 kubelet-1.20.5 --disableexcludes=kubernetes
 systemctl enable --now kubelet
